6ef0f3c8ca13ada6749b747554116af48a4456fe
Deam Diaz <jdamian27@gmail.com>
2019-07-02 14:51:08 -0400

    Define rule and start job when agenda is ready

--- methods/actions.js
+++ methods/actions.js
@@ -7,7 +7,28 @@ var functions = {
     start: function (req, res) {
         const connectionOpts = {db: {address: 'mongodb://localhost:27017/schedule'}};
         const agenda = new Agenda(connectionOpts);
-        agenda.processEvery('one minute');
+        agenda.define('save user', (job, done) => {
+            const time = new Date();
+            console.log(`\n#### ${time.toTimeString()}`);
+            console.log("started...");
+            let {domain} = job.attrs.data;
+            let conn = mongoose.createConnection(config.database),
+                User = conn.model('User', user);
+            console.log("getting data...");
+            User.find({"email": {$regex: `@${domain}`}}).then((data) => {
+                data.forEach((elem, index) => {
+                    console.log(`${elem.name} - ${elem.email}`);
+                    console.log(`########`);
+                });
+            });
+            done();
+        });
+        agenda.on('ready', () => {
+            agenda.every('one minute', 'save user', {domain: 'asd.com'});
+            agenda.start();
+            agenda.now('save user', {domain: 'asd.com'});
+            res.status(200).send("scheduled..");
+        });
     }
 };
 module.exports = functions;
